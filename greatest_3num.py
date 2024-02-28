a1c63b52df12cee19fd80981c767a61d7c01b546
Durgaj09 <159923696+Durgaj09@users.noreply.github.com>
2024-02-28 21:54:21 +0530

    Add files via upload

---
+++ greatest_3num.py
@@ -0,0 +1,11 @@
+a,b,c=input().split(" ")
+if(a>b):
+    if(a>c):
+        print("a greater")
+    else:
+        print("c is greater")
+else:
+    if(b>c):
+        print("b greater")
+    else:
+        print("c is greater")
